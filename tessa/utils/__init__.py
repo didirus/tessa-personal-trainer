89ca82b44ebb841e739d279058eb0986f30ecb95
Diede Rusticus <diede@omnius.com>
2020-10-20 15:01:45 +0200

    set loggerlevel to info by default

--- tessa/utils/__init__.py
+++ tessa/utils/__init__.py
@@ -1,7 +1,7 @@
 # Using try except to give to not get a error while building wheel without reconplogger
 try:
     import reconplogger
-    logger = reconplogger.logger_setup()
+    logger = reconplogger.logger_setup(level='INFO')
 
 except ImportError:
     print("*********reconplogger missing********")
